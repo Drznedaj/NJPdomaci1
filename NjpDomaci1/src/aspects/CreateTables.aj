720f5ff6b51de92ba097fddac3c97faf526b5c12
maxosacc <igor.maxsimovic@gmail.com>
2018-11-03 17:21:33 +0100

    evo inita majko

--- NjpDomaci1/src/aspects/CreateTables.aj
+++ NjpDomaci1/src/aspects/CreateTables.aj
@@ -1,11 +1,19 @@
 package aspects;
 
+import api.DAO;
+import api.ORM;
+import entities.Proizvod;
+
 public aspect CreateTables{
 	
 	pointcut initTables() : initialization(ORM.new(..));
+	pointcut insertTables() : initialization(ORM.new(..));
 	
-	before() : initTables() {
+	after() : initTables() {
 		// TODO kreirati entitete i napuniti magijom tabele, create , insert.
+		
+		
+		
 	}
 //	pointcut createTables() : initialization(* type_pattern.new(..)) ;
 }
--- NjpDomaci1/src/test/Main.java
+++ NjpDomaci1/src/test/Main.java
@@ -1,5 +1,6 @@
 package test;
 
+import api.DAO;
 import api.ORM;
 import entities.Proizvod;
 
@@ -7,17 +8,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Proizvod p = new Proizvod();
-		ORM orm = new ORM();
 		
-        System.out.println(orm.getTableName(p.getClass()));
-        System.out.println(orm.getTableColumns(p.getClass()));
-        String koloneFormat = "(";
-		for (String s: orm.getTableColumns(p.getClass())
-			 ) {
-			koloneFormat += s+",";
-		}
-		koloneFormat+=")";
-		System.out.printf("INSERT INTO %s %s VALUES (%s)",orm.getTableName(p.getClass()),koloneFormat,"valuesFormat");
+		DAO.getInstance();
+//        System.out.println(DAO.getInstance().getTableName(p.getClass()));
+//        System.out.println(DAO.getInstance().getTableColumns(p.getClass()));
+		
 //        System.out.println(orm.getSuperClassColumns(p.getClass().getSuperclass()));
 	}
 
